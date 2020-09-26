913262990a8852aadfa43a0b9acaf2d7459b3a3a
Vinicius <vinicius.almeida.r@gmail.com>
2020-09-26 12:38:05 -0300

    DOCS: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,12 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
